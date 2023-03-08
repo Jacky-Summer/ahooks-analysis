8e7f846c4920bc5e6794aa3ffb3e3ea155a6fd62
JackySummer <1106619871@qq.com>
2023-03-08 22:28:59 +0800

    docs: useEventEmitter

--- .umirc.ts
+++ .umirc.ts
@@ -63,6 +63,7 @@ export default defineConfig({
           'hooks/dom/useFocusWithin',
           'hooks/advanced/useControllableValue',
           'hooks/advanced/useCreation',
+          'hooks/advanced/useEventEmitter',
         ],
       },
     ],
